37f5290a5109fadc4a873da5db76d82df336e838
popescuedi99 <80031022+popescuedi99@users.noreply.github.com>
2023-04-07 09:40:40 +0300

    started B1

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var07/PracticalTest01Var07MainActivity.java
@@ -3,12 +3,28 @@ package ro.pub.cs.systems.eim.practicaltest01var07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest01Var07MainActivity extends AppCompatActivity {
 
+    private EditText leftTopEditText;
+    private EditText rightTopEditText;
+    private EditText leftBotEditText;
+    private EditText rightBotEditText;
+    private Button setButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_practical_test01_var07_main);
+
+        super.onCreate(savedInstanceState);
+        leftTopEditText=(EditText)findViewById(R.id.left_top_edit_text);
+        rightTopEditText=(EditText)findViewById(R.id.right_top_edit_text);
+        leftBotEditText=(EditText)findViewById(R.id.left_bot_text);
+        rightBotEditText=(EditText)findViewById(R.id.right_bot_text);
+        setButton = (Button)findViewById(R.id.Set);
+        setContentView(R.layout.activity_practical_test01_var07_main);
+
     }
 }
